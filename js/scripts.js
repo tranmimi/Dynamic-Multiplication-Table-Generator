b0bce3171a993dc1cb58339e99e901fbc226963f
Mimi Tran <74216996+tranmimi@users.noreply.github.com>
2023-01-15 21:24:23 -0500

    HW 3 portion completed.
    Features implemented:
    - A working form that accepts only integers in the range [-50, 50]. Also error checks for minimum values that are larger than the maximum values.
    - Correct multiplication tables are generated based on input provided in the form.
    - New inputs will cause the previous table (if generated) to be replaced with the new table.

---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+/*
+    Name: Mimi Tran
+    Last Updated: January 13, 2023
+    HW 3: Creating an Interactive Dynamic Table
+    HW 4: Using the jQuery Plugin/UI with Your Dynamic Table
+    validation.js
+*/
+
+/* 
+    validateInputs grabs user input from the form and checks the following:
+        - minRowVal <= maxRowVal
+        - minColVal <= maxColVal
+
+    NOTE: Error handling for out-of-bounds, string, and decimal inputs are handled via 
+            HTML5 validation checking. 
+*/
+
+function validateInputs(event) {
+    // Clear any previous error messages if user submits inputs more than once.
+    document.getElementById("errorMinR").innerHTML = "";
+    document.getElementById("errorMinC").innerHTML = "";
+
+    // Retrieve user input submitted in the form.
+    const minRowVal = parseInt(document.getElementById("minRow").value);
+    const maxRowVal = parseInt(document.getElementById("maxRow").value);
+    const minColVal = parseInt(document.getElementById("minCol").value);
+    const maxColVal = parseInt(document.getElementById("maxCol").value);
+
+    // Create a multiplication table using the entered values if they are valid.
+    if (minRowVal <= maxRowVal && minColVal <= maxColVal) {
+        createMultTable(minRowVal, maxRowVal, minColVal, maxColVal);
+    } else {
+        // If the minimum row value is larger than the maximum row value, inform the user.
+        if (minRowVal > maxRowVal) {
+            document.getElementById("errorMinR").innerHTML = "*" + minRowVal + " is larger than " 
+                + maxRowVal + ". Please enter an integer less than or equal to " + maxRowVal 
+                + ".";
+        // If the minimum column value is larger than the maximum column value, inform the user.
+        } 
+        if (minColVal > maxColVal) {
+            document.getElementById("errorMinC").innerHTML = "*" + minColVal + " is larger than " 
+            + maxColVal + ". Please enter an integer less than or equal to " + maxColVal 
+            + ".";
+        }
+    }
+}
+
+/*
+    createMultTable will create a multiplication table using valid parameters given
+    by the user.
+*/
+function createMultTable(minR, maxR, minC, maxC) {
+    var currCol = minC, ctemp = currCol;
+    var currRow = minR, rtemp = currRow;
+    var row = 0, col = 0, cellData;
+
+    // Create a Table element and Table Body element for the multiplication table.
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    // Construct the multiplication table 
+    for (row = 0; row <= (Math.abs(maxR - minR) + 1); row++) {
+        // Create new row in the table.
+        const newRow = document.createElement("tr");
+        for (col = 0; col <= (maxC - minC + 1); col++) {
+            const cell = document.createElement("td");
+            // Uppermost, leftmost cell stays blank.
+            if (row == 0 && col == 0) {
+                cellData = document.createTextNode(" ");
+            // If a cell is added along the first row or column, the cell will hold 
+            // the appropriate row or column number--not any calculated products.
+            } else if (row == 0 && col != 0) {
+                cellData = document.createTextNode(ctemp);
+                ctemp++;
+            } else if (row != 0 && col == 0) {
+                cellData = document.createTextNode(rtemp);
+                rtemp++;
+            // Otherwise, compute the product between the current multiplicand and multiplier, and save this product into the current cell being made. 
+            } else {
+                cellData = document.createTextNode(currRow * currCol);
+                currCol++;
+            } 
+            cell.appendChild(cellData);
+            newRow.appendChild(cell);
+        }
+        tblBody.appendChild(newRow); // Append this newly completed row to the table body.
+        if (row != 0) {
+            currRow++;
+        }
+        currCol = minC; // Reset currCol for each column iteration
+    }
+
+    // Insert newly filled table body into the newly created table.
+    tbl.appendChild(tblBody);
+    
+    // Replaces an existing table with new one.
+    var isThereAPrevTable = document.getElementById("multTable");
+    if(isThereAPrevTable != null) {
+        const prevTable = document.getElementById("multTable");
+        prevTable.replaceWith(tbl);
+    // If a multiplication table did not previously exist, simply append the newly created table to <body>
+    } else {
+        document.body.appendChild(tbl);
+    }
+
+    tbl.setAttribute("id", "multTable");
+}
